51909076af5207d47a01da2c1402ca82fe0c1694
JulianColoma <juliancoloma751@gmail.com>
2025-08-24 00:09:46 -0300

    initial commit

---
+++ metro.config.js
@@ -0,0 +1,20 @@
+// metro.config.js
+const { getDefaultConfig } = require("expo/metro-config");
+
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    // Usa el transformer especial para Expo
+    babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"],
+  };
+
+  return config;
+})();
