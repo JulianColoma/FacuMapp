d210389963bfc432bb285d3eecc63b86e41fcbf4
Hajime Shiroma <hajishiro.hs@gmail.com>
2025-12-01 10:59:47 -0300

    feat: refactor InteractiveMap to use centralized color constants and improve code readability
    - Introduced a new colors.tsx file to define color constants for the application.
    - Updated InteractiveMap component to utilize the new color constants for SVG elements.
    - Enhanced code formatting and readability in InteractiveMap by restructuring JSX and improving comments.
    - Added type annotations for better TypeScript support.

---
+++ constants/colors.tsx
@@ -0,0 +1,19 @@
+export const COLORS = {
+  verde: '#38dc26ff',
+  verde_claro: '#e2fee3ff',
+  gris: '#afafafff',
+  blanco: '#ffffffff',
+
+} as const;
+
+// Opcional: combinaciones específicas para espacios
+export const SPACE_COLORS = {
+  default: {
+    fill: COLORS.verde_claro,
+    stroke: COLORS.verde,
+  },
+  aula: {
+    fill: COLORS.verde_claro,
+    stroke: COLORS.verde,
+  },
+} as const;
